5e682621a19c2cd90b1cba1c1426f995399d2815
Samuel <fridaydamilare22@gmail.com>
2022-10-21 16:10:12 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const image = document.querySelectorAll(".image img")
+
+image.forEach(img => {
+    img.addEventListener("click", () => {
+        document.querySelector(".modelBox").style.display = 'block';
+        document.querySelector(".modelBox .image img").src = img.getAttribute("src")
+    })
+})
+
+const close = document.querySelector(".close")
+
+close.addEventListener("click", () => {
+    document.querySelector(".modelBox").style.display = 'none'
+})
